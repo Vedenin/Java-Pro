dbc08e8c96c61b6dc475ab387682263561549e12
Svitlana Graf <svetlanapss@gmail.com>
2022-08-01 22:04:33 +0200

    added gitignor

--- src/main/java/hw_1/Main.java
+++ src/main/java/hw_1/Main.java
@@ -15,11 +15,11 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Employee[] employeesTable = new Employee[5];
-		employeesTable[0] = new Employee("Ivanov Ivan Ivanovich", "Director", "ivanov@mail.me", "0301233333", 60000, 62);
-		employeesTable[1] = new Employee("Petrov Petr Petrovich", "Driver", "petrov@mail.me", "0301231212", 15000, 19);
-		employeesTable[2] = new Employee("Sidorov Sidor Sidorovich", "Accountant", "sidorov@mail.me", "301231323", 30000, 42);
-		employeesTable[3] = new Employee("Ivanova Ivanka Ivanovna", "Economist", "ivanov@mail.me", "301232313", 35000, 21);
-		employeesTable[4] = new Employee("Petrova Elena Semenovna", "Lawyer", "petrova@mail.me", "301230023", 55000, 46);
+		employeesTable[0] = new Employee("Ivanov Ivan Ivanovich", "Director", "ivanov@mail.com", "11111111111", 6000, 62);
+		employeesTable[1] = new Employee("Petrov Petr Petrovich", "Driver", "petrov@mail.com", "22222222222", 3200, 18);
+		employeesTable[2] = new Employee("Sidorov Sidor Sidorovich", "Accountant", "sidorov@mail.com", "33333333333", 3300, 42);
+		employeesTable[3] = new Employee("Ivanova Ivanka Ivanovna", "Economist", "ivanov@mail.com", "44444444444", 4400, 31);
+		employeesTable[4] = new Employee("Petrova Elena Semenovna", "Lawyer", "petrova@mail.com", "55555555555", 5500, 56);
 		
 		for (Employee employee : employeesTable) {
 			if (employee.getAge() > 40) employee.printInfo();
