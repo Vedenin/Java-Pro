2c1369a04aa975770582fb1597b673d82ea514f8
Svitlana Graf <svetlanapss@gmail.com>
2022-08-08 23:13:19 +0200

    Pizza task

--- src/main/java/hw3/Main.java
+++ src/main/java/hw3/Main.java
@@ -7,25 +7,27 @@ Create a PizzaOrder class where pizza can be added using the add(Pizza) method a
 write a calcTotalPrice(): int method that will calculate the total order amount for all pizzas.
  */
 
+import static java.lang.System.*;
+
 public class Main {
 	public static void main(String[] args) {
 		
 		PizzaOrder pizzaOrder1 = new PizzaOrder();
 		pizzaOrder1.addPizza("s", 2, 2, 2);
-		System.out.println("Order: " + pizzaOrder1.getCount() + " pizza cost " + pizzaOrder1.calcTotalPrice());
+		out.printf("Order: %d pizza cost %d%n", pizzaOrder1.getCount(), pizzaOrder1.calcTotalPrice());
 
 		pizzaOrder1.addPizza("l", 0, 4, 1);
-		System.out.println("+ Order: " + pizzaOrder1.getCount() + " pizza(s) cost(s) " + pizzaOrder1.calcTotalPrice());
+		out.printf("+ Order: %d pizza(s) cost(s) %d%n", pizzaOrder1.getCount(), pizzaOrder1.calcTotalPrice());
 		
 		PizzaOrder pizzaOrder2 = new PizzaOrder();
 		pizzaOrder2.addPizza("s", 2, 2, 2);
-		System.out.println("Order: " + pizzaOrder2.getCount() + " pizza cost " + pizzaOrder2.calcTotalPrice());
+		out.printf("Order: %d pizza cost %d%n", pizzaOrder2.getCount(), pizzaOrder2.calcTotalPrice());
 		
 		pizzaOrder2.addPizza("m", 4, 0, 0);
-		System.out.println("+ Order: " + pizzaOrder2.getCount() + " pizza(s) cost(s) " + pizzaOrder2.calcTotalPrice());
+		out.printf("+ Order: %d pizza(s) cost(s) %d%n", pizzaOrder2.getCount(), pizzaOrder2.calcTotalPrice());
 		
 		pizzaOrder2.addPizza("l", 1, 1, 2);
-		System.out.println("+ Order: " + pizzaOrder2.getCount() + " pizza(s) cost(s) " + pizzaOrder2.calcTotalPrice());
+		out.printf("+ Order: %d pizza(s) cost(s) %d%n", pizzaOrder2.getCount(), pizzaOrder2.calcTotalPrice());
 
 	}
 }
