02969f83822cc23bcd88da160f9f881a1574c7f8
Svitlana Graf <svetlanapss@gmail.com>
2022-08-31 12:37:59 +0200

    second solutions

--- src/main/java/hw10_LinkedList/Main.java
+++ src/main/java/hw10_LinkedList/Main.java
@@ -9,20 +9,21 @@ to the right by k positions. For example, 1 - 2 - 3 - 4 - 5 - 6 at k = 1 is conv
 and at k = 3 to 4 - 5 - 6 - 1 - 2 - 3.
  */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> one = new LinkedList<>(Arrays.asList(1, 4, 5, 10));
         LinkedList<Integer> two = new LinkedList<>(Arrays.asList(1, 2, 3, 8, 11, 21));
-        System.out.println(joinedLists(one, two));
+        System.out.println(joinedLists(one, two)); // using an iterator
+        System.out.println(mergeLists(one, two)); // using merge .add
 
-        ArrayList<Integer> three = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
-        int k = 3;
+        List<Integer> three = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int k = 9;
         System.out.println(shiftedArray(three, k));
+        System.out.println(rotateList(three, k));
+
+
     }
 
     private static LinkedList<Integer> joinedLists(LinkedList<Integer> one, LinkedList<Integer> two) {
@@ -71,13 +72,33 @@ public class Main {
         }
     }
 
-    private static ArrayList<Integer> shiftedArray (ArrayList<Integer> list, int k) {
-        ArrayList<Integer> result = new ArrayList<>();
+    private static List<Integer> mergeLists(LinkedList<Integer> one, LinkedList<Integer> two) {
+        List<Integer> result = new LinkedList<>();
+        int i = 0, j = 0;
+        while (i < one.size() && j < two.size()) {
+            if (one.get(i) < two.get(j)) {
+                result.add(one.get(i));
+                i++;
+            } else {
+                result.add(two.get(j));
+                j++;
+            }
+        }
+        if (i < one.size()) {
+            result.addAll(one.subList(i, one.size()));
+        }
+        if (j < two.size()) {
+            result.addAll(two.subList(j, two.size()));
+        }
+        return result;
+    }
+
+    private static List<Integer> shiftedArray(List<Integer> list, int k) {
+       ArrayList<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < list.size(); i++) {
             result.add(0);
         }
-
         for (int i = 0; i < list.size(); i++) {
             result.set((i + k) % list.size(), list.get(i));
 
@@ -85,4 +106,18 @@ public class Main {
         return result;
     }
 
+    private static List<Integer> rotateList(List<Integer> list, int k) {
+
+        for (int i = 0; i < k; i++) {
+
+            int last = list.size() - 1;
+            int temp = list.get(last);
+
+            for (int j = last; j > 0; j--) {
+                list.set(j, list.get(j - 1));
+            }
+            list.set(0, temp);
+        }
+        return list;
+    }
 }
